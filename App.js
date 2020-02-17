233f954579cea957c2ab50dd34b2c133f182cf81
Adarsh Nanwani <adarsh.nanwani@gmail.com>
2020-02-17 22:40:52 +0530

    RNVI linking and delete functionality for items.

--- App.js
+++ App.js
@@ -1,12 +1,45 @@
-import React from 'react';
-import {View, StyleSheet} from 'react-native';
+import React, {useState} from 'react';
+import {View, StyleSheet, FlatList} from 'react-native';
+import uuid from 'uuid/v4';
 import Header from './src/components/Header';
+import ListItem from './src/components/ListItem';
 
-const App = () => (
-  <View style={styles.container}>
-    <Header />
-  </View>
-);
+const App = () => {
+  const [items, setItems] = useState([
+    {
+      id: uuid(),
+      text: 'Milk',
+    },
+    {
+      id: uuid(),
+      text: 'Bread',
+    },
+    {
+      id: uuid(),
+      text: 'Eggs',
+    },
+    {
+      id: uuid(),
+      text: 'Juice',
+    },
+  ]);
+
+  const handleDelete = id => {
+    const otherItems = items.filter(item => item.id !== id);
+    setItems(otherItems);
+  };
+  return (
+    <View style={styles.container}>
+      <Header />
+      <FlatList
+        data={items}
+        renderItem={({item}) => (
+          <ListItem item={item} deleteItem={handleDelete} />
+        )}
+      />
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
---
+++ src/components/ListItem.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import {View, Text, TouchableOpacity, StyleSheet} from 'react-native';
+import Icon from 'react-native-vector-icons/dist/FontAwesome';
+
+const ListItem = ({item, deleteItem}) => {
+  const {text, id} = item;
+  return (
+    <TouchableOpacity style={styles.listItem} onPress={() => deleteItem(id)}>
+      <View style={styles.listItemView}>
+        <Text style={styles.listItemText}>{text}</Text>
+        <Icon name="remove" size={20} color="firebrick" />
+      </View>
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({
+  listItem: {
+    padding: 15,
+    backgroundColor: '#f8f8f8',
+    borderBottomWidth: 1,
+    borderColor: '#eee',
+  },
+  listItemView: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  listItemText: {
+    fontSize: 18,
+  },
+});
+
+export default ListItem;
